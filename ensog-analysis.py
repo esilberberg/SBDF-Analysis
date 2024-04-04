0a2a4d5e277be62563285aa100c0d172b1e17326
esilberberg <ericsil218@gmail.com>
2024-04-04 12:49:03 -0400

    fixed dupe error in dewey ensog analysis

--- ensog-analysis.py
+++ ensog-analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 ENSOG_file = 'ENSOG Library Catalog Sala de lectura.xlsx'
-dewey_codes_file = 'dewey-codes.xlsx'
+dewey_codes_file = 'code_constructors/dewey-codes.xlsx'
 
 df = pd.read_excel(ENSOG_file)
 dewey_codes_df = pd.read_excel(dewey_codes_file)
@@ -40,35 +40,22 @@ def lookup_dewey_class(dewey_number):
 
 
 df['dewey_class'] = df['signatura-topografica'].apply(format_dewey_number)
-df['description'] = df['dewey_class'].apply(lookup_dewey_class)
+dewey_class_count = df['dewey_class'].value_counts()
 
-dewey_class_counts = df['dewey_class'].value_counts()
-description_counts = df['description'].value_counts()
-
-df_dewey_class = pd.DataFrame({'dewey_class': dewey_class_counts.index, 'count': dewey_class_counts.values})
-df_description = pd.DataFrame({'description': description_counts.index, 'count': description_counts.values})
-
-books_by_dewey_class_df = pd.merge(df_dewey_class, df_description, on='count')
+books_by_dewey_class_df = pd.DataFrame({
+    'Dewey_code': dewey_class_count.index,
+    'Count': dewey_class_count.values,
+    'Description': dewey_class_count.index.map(lookup_dewey_class)
+})
 
 books_by_author = df['autor'].value_counts()
 books_by_publisher = df['editorial'].value_counts()
 
-
 print(books_by_dewey_class_df)
 print(books_by_author)
 print(books_by_publisher)
-print(f'Total books: {df.size}')
-
 
 with pd.ExcelWriter('ensog_analysis.xlsx') as writer:
     books_by_dewey_class_df.to_excel(writer, sheet_name='Class', index=False)
     books_by_author.to_excel(writer, sheet_name='Author')
     books_by_publisher.to_excel(writer, sheet_name='Publisher')
-
-
-
-
-
-
-
-
